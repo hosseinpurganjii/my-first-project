192bbcd7abcddd3b812f2f3c96a61f149bd81b77
hosseinpurganji <hosseinpurganji@gmail.com>
2025-10-04 04:04:39 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,4 +7,8 @@ setup(
     author="Hossein",
     description="A simple library",
     author_email="hosseinpurganji.com",
+
 )
+
+
+#اینجا بعد از انجام دادن مراحل ساخت پکیج اونو با دستور     pip install . پکیج رو نصب میکنیم و دیگه از این ب بعد میتونیم خارج از پوشه هم از ازش استفاده کنیم
