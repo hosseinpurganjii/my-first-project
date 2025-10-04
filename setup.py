dc246820ae3ef097ab6c98542ba267df35dc6c3a
hosseinpurganji <hosseinpurganji@gmail.com>
2025-10-04 03:46:55 +0330

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hossein",
+    version="0.1",
+    packages=find_packages(),
+    author="Hossein",
+    description="A simple library",
+    author_email="hosseinpurganji.com",
+)
